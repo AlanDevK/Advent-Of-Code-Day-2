0fc3ba7f475c47e81893f9483b4c1781dee4e892
Kazu <140027976+AlanDevK@users.noreply.github.com>
2024-12-02 22:23:14 +0700

    Adding solution code onto Github
    With input data and output/solution

---
+++ DAY2.py
@@ -0,0 +1,33 @@
+fi = open('D:/Phuong/Code/Python/PRACTICEAGAIN/ADVENTOFCODEDAY2/DAY2.inp')
+reports = fi.readlines()
+fi.close()
+count = 0
+def safeReportFilter(report):
+    state = ""
+    levelReport = list(map(int, report.split(" ")))
+    for i in range(1, len(levelReport)):
+        if levelReport[i] < levelReport[i-1]:
+            if state == "INCREASE" or abs(levelReport[i] - levelReport[i-1])>3:
+                return "UNSAFE"
+            elif state == "":
+                if abs(levelReport[i] - levelReport[i-1]) > 3:
+                    return "UNSAFE"
+                else:
+                    state = "DECREASE"
+        elif levelReport[i] > levelReport[i-1]:
+            if state == "DECREASE" or abs(levelReport[i] - levelReport[i-1])>3:
+                return "UNSAFE"
+            elif state == "":
+                if abs(levelReport[i] - levelReport[i-1]) > 3:
+                    return "UNSAFE"
+                else:
+                    state = "INCREASE"
+        elif levelReport[i] == levelReport[i-1]:
+            return "UNSAFE"
+    return "SAFE"
+for report in reports:
+    if (safeReportFilter(report) == "SAFE"):
+        count+=1
+fo = open('D:/Phuong/Code/Python/PRACTICEAGAIN/ADVENTOFCODEDAY2/DAY2.out','w')
+fo.write(str(count))
+fo.close()
