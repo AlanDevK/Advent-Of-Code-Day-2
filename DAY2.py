dddb0e87744be339ca6067b859d00faff2ea4ab8
Kazu <140027976+AlanDevK@users.noreply.github.com>
2024-12-04 14:55:36 +0700

    Update DAY2.py
    Spaghetti/Unfinished solution code with errors need to be solved.

--- DAY2.py
+++ DAY2.py
@@ -3,31 +3,65 @@ reports = fi.readlines()
 fi.close()
 count = 0
 def safeReportFilter(report):
-    state = ""
-    levelReport = list(map(int, report.split(" ")))
+    global errorIndex
     for i in range(1, len(levelReport)):
-        if levelReport[i] < levelReport[i-1]:
-            if state == "INCREASE" or abs(levelReport[i] - levelReport[i-1])>3:
+        if i+1<len(levelReport):
+            if (levelReport[i] < levelReport[i-1] and levelReport[i] < levelReport[i+1]):
+                print(levelReport[i] < levelReport[i-1] and levelReport[i] < levelReport[i+1])
+            if (abs(levelReport[i] - levelReport[i-1]) > 3):
+                print(abs(levelReport[i] - levelReport[i-1]) > 3)
+            if (abs(levelReport[i] - levelReport[i+1]) > 3):
+                print(abs(levelReport[i] - levelReport[i+1]) > 3)
+            if (levelReport[i] > levelReport[i-1] and levelReport[i] > levelReport[i+1]):
+                print(levelReport[i] > levelReport[i-1] and levelReport[i] > levelReport[i+1])
+            if (levelReport[i] == levelReport[i-1] or levelReport[i] == levelReport[i+1]):
+                print(levelReport[i] == levelReport[i-1] or levelReport[i] == levelReport[i+1])
+            if (levelReport[i] < levelReport[i-1] and levelReport[i] < levelReport[i+1]) or abs(levelReport[i] - levelReport[i-1]) > 3 or abs(levelReport[i] - levelReport[i+1]) > 3:
+                errorIndex = i
                 return "UNSAFE"
-            elif state == "":
-                if abs(levelReport[i] - levelReport[i-1]) > 3:
-                    return "UNSAFE"
-                else:
-                    state = "DECREASE"
-        elif levelReport[i] > levelReport[i-1]:
-            if state == "DECREASE" or abs(levelReport[i] - levelReport[i-1])>3:
+            elif (levelReport[i] > levelReport[i-1] and levelReport[i] > levelReport[i+1]) or abs(levelReport[i] - levelReport[i-1]) > 3 or abs(levelReport[i] - levelReport[i+1]) > 3:
+                errorIndex = i
                 return "UNSAFE"
-            elif state == "":
-                if abs(levelReport[i] - levelReport[i-1]) > 3:
-                    return "UNSAFE"
-                else:
-                    state = "INCREASE"
-        elif levelReport[i] == levelReport[i-1]:
-            return "UNSAFE"
+            elif (levelReport[i] == levelReport[i-1] or levelReport[i] == levelReport[i+1]):
+                errorIndex = i
+                return "UNSAFE"
+        else:
+            if (abs(levelReport[i] - levelReport[i-1]) > 3):
+                print(abs(levelReport[i] - levelReport[i-1]) > 3)
+            if (levelReport[i] == levelReport[i-1]):
+                print(levelReport[i] == levelReport[i-1])
+            if abs(levelReport[i] - levelReport[i-1]) > 3:
+                errorIndex = i
+                return "UNSAFE"
+            elif (levelReport[i] == levelReport[i-1]):
+                errorIndex = i
+                return "UNSAFE"
     return "SAFE"
+def getErrorIndex():
+    global errorIndex
+    return errorIndex
 for report in reports:
-    if (safeReportFilter(report) == "SAFE"):
+    levelReport = list(map(int, report.split(" ")))
+    tempLevelReport = list(map(int, report.split(" ")))
+    print(levelReport)
+    print(safeReportFilter(levelReport))
+    if (safeReportFilter(levelReport) == "SAFE"):
         count+=1
+    elif (safeReportFilter(levelReport) == "UNSAFE"):
+        print("ITERATION 2")
+        savedErrorIndex = getErrorIndex()
+        tempLevelReport.remove(tempLevelReport[savedErrorIndex])
+        print(tempLevelReport)
+        print(safeReportFilter(tempLevelReport))
+        if (safeReportFilter(tempLevelReport) == "SAFE"):
+            count+=1
+        elif (safeReportFilter(tempLevelReport) == "UNSAFE" and savedErrorIndex+1<len(levelReport)):
+            print("ITERATION 3")
+            levelReport.remove(levelReport[savedErrorIndex+1])
+            print(levelReport)
+            print(safeReportFilter(levelReport))
+            if (safeReportFilter(levelReport) == "SAFE"):
+                count+=1
 fo = open('DAY2.out','w')
 fo.write(str(count))
 fo.close()
